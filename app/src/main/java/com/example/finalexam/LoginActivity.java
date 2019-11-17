cc3f2eb626a564704ca4f83f090cba8bfe602f41
snowsnowaum01 <snowsnowaum01@gmail.com>
2019-11-17 11:42:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/finalexam/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.finalexam;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.finalexam.DB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private DB db;
+    private List<RegisterActivity> List = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        db = new DB(LoginActivity.this);
+        List.addAll(db.getAllNotes());
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userEditText = findViewById(R.id.username_edit_text);
+                EditText passwordEditText = findViewById(R.id.password_edit_text);
+                if(userEditText.getText().toString().equals("")||passwordEditText.getText().toString().equals("")){
+                    Toast.makeText(LoginActivity.this,"All fields are required",Toast.LENGTH_SHORT).show();
+                }
+                else if(List.size()==0){
+                    Toast.makeText(LoginActivity.this,"Invalid username or password",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    for(int i = 0;i<List.size();i++){
+                        if(List.get(i).equals(userEditText.getText().toString())){
+                            if(List.get(i).equals(passwordEditText.getText().toString())){
+                                Toast.makeText(LoginActivity.this,"Welcome"+" "+List.get(i).getName(),Toast.LENGTH_SHORT).show();
+                                break;
+                            }
+                            else{
+                                Toast.makeText(LoginActivity.this,"Invalid username or password",Toast.LENGTH_SHORT).show();
+                                break;
+                            }
+                        }
+                        if(i==List.size()){
+                            Toast.makeText(LoginActivity.this,"Invalid username or password",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+        findViewById(R.id.register_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/finalexam/RegisterActivity.java
@@ -0,0 +1,107 @@
+package com.example.finalexam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterActivity extends AppCompatActivity {
+    public static final String TABLE_NAME = "userTable";
+
+    public static final String COLUMN_USER = "Username";
+    public static final String COLUMN_NAME = "FullName";
+    public static final String COLUMN_PASS = "Pass";
+
+    private String user;
+    private String name;
+    private String pass;
+
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_USER + " TEXT PRIMARY KEY "
+                    + COLUMN_NAME + " TEXT,"
+                    + COLUMN_PASS + " TEXT"
+                    + ")";
+
+    public RegisterActivity() {
+    }
+
+    public RegisterActivity(String id, String note, String timestamp) {
+        this.user = id;
+        this.name = note;
+        this.pass = timestamp;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        findViewById(R.id.register_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean dupli = false;
+                List<RegisterActivity> List = new ArrayList<>();
+                DB db= new DB(RegisterActivity.this);
+                EditText userEditText = findViewById(R.id.username_edit_text);
+                EditText passwordEditText = findViewById(R.id.password_edit_text);
+                EditText nameEditText = findViewById(R.id.full_name_edit_text);
+                if(userEditText.getText().toString().equals("")||passwordEditText.getText().toString().equals("")||nameEditText.getText().toString().equals("")){
+                    Toast.makeText(RegisterActivity.this,"All fields are required",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(List.size()==0){
+                        db.insertName(userEditText.getText().toString(),nameEditText.getText().toString(), passwordEditText.getText().toString());
+                        Toast.makeText(RegisterActivity.this,"Register successfully",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        for(int i = 0;i<List.size();i++){
+                            if(List.get(i).equals(userEditText.getText().toString())){
+                                Toast.makeText(RegisterActivity.this,"Duplicate Username",Toast.LENGTH_SHORT).show();
+                                dupli = true;
+                                break;
+                            }
+                            else{
+                                dupli = false;
+                            }
+                        }
+                        if(dupli==false){
+                            db.insertName(userEditText.getText().toString(),nameEditText.getText().toString(), passwordEditText.getText().toString());
+                            Toast.makeText(RegisterActivity.this,"Register successfully",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
